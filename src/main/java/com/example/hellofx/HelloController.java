b62776596234ac31afef7f44e1add2bc92d95b6a
Idil Abdikadir <170156028@aston.ac.uk>
2023-09-05 22:12:44 +0100

    changed .gitignore

--- src/main/java/com/example/hellofx/HelloController.java
+++ src/main/java/com/example/hellofx/HelloController.java
@@ -1,14 +1,93 @@
 package com.example.hellofx;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable{
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    private Label songName;
+    private File [] files;
+    List<File> songs;
+    private File directory;
+    private int songIndex;
+    private Media media;
+    private MediaPlayer mediaPlayer;
+    private File currentSongFile;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        songs = new ArrayList<>();
+        songIndex = 0;
+        directory = new File("musically");
+        files = directory.listFiles();
+        if (files != null) {
+            for(File file: files) {
+                songs.add(file);
+            }
+        }
+
+
+
+        File currentSongFile = songs.get(songIndex);
+        songName.setText(currentSongFile.getName());
+        media = new Media(currentSongFile.toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+
     }
+
+    public void play() {
+        mediaPlayer.play();
+    }
+
+    public void pause() {
+
+        mediaPlayer.pause();
+    }
+
+    public void next() {
+        mediaPlayer.stop();
+        songIndex++;
+        if(songIndex > songs.size()-1) {
+            songIndex = 0;
+        }
+
+        currentSongFile = songs.get(songIndex);
+        songName.setText(currentSongFile.getName());
+        media = new Media(currentSongFile.toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+    }
+
+    public void previous() {
+        mediaPlayer.stop();
+        songIndex--;
+        if(songIndex < 0) {
+            songIndex = songs.size() -1;
+        }
+
+        currentSongFile = songs.get(songIndex);
+        songName.setText(currentSongFile.getName());
+        media = new Media(currentSongFile.toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+
+    }
+
+
+
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.hellofx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
 
 
     opens com.example.hellofx to javafx.fxml;
